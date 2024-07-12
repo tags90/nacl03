b12ea024627e666033eeba4986baee5dc2dd6133
tags90 <nguyencaonhan480@gmail.com>
2024-07-12 20:49:10 +0700

    07/12/2024 - sửa làn 2

--- info/aboutme.py
+++ info/aboutme.py
@@ -1,2 +1,4 @@
 def about():
-    print("Tools Create by NguyenCaoNhan :)))))))))))))))))))\n")
+    print("Tìm làm cái gì lo học đê")
+    print("Tools create by NguyenCaoNhan. License by GNU Public")
+    print("\n")
